534a6fee11c595f2528272afb3709115deefdace
erozzae <rosihann14@mail.ugm.ac.id>
2024-07-23 18:08:59 +0700

    update

--- App.py
+++ App.py
@@ -2,20 +2,9 @@ import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
 
-# def connect():
-#     # Initialize connection.
-#     conn = st.connection('mysql', type='sql')
-#     st.info("hehe")
-#     Contoh query untuk mengambil data dari tabel
-#     df = conn.query('SELECT * from well_a Limit 10;', ttl=600)
-    
-#     # Mengambil data dari database
-#     for row in df.itertuples():
-#         st.write(f"{row.BitDepth} has a :{row.Hkld}:")
-# connect()
 
 def connect_sql():
-    connection_string = f'mysql+pymysql://{st.secrets["username"]}:{st.secrets["password"]}@{st.secrets["host"]}/{st.secrets["database"]}'
+    connection_string = f'mysql+pymysql://{st.secrets["username"]}:{st.secrets["password"]}@{st.secrets["host"]}:{st.secrets["port"]}/{st.secrets["database"]}'
     return create_engine(connection_string)
 
 @st.cache_data
@@ -32,6 +21,8 @@ def main():
     if st.button("Jalankan Query"):
         data = load_data(query)
         st.write(data)
+
+
 
 if __name__ == "__main__":
     main()
