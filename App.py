7d3f7ffe92c3bec96287ae20afa49fe155bc27b4
erozzae <rosihann14@mail.ugm.ac.id>
2024-07-23 19:04:08 +0700

    upd

--- App.py
+++ App.py
@@ -1,16 +1,23 @@
 import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
+import mysql.connector
+
+# def connect_sql():
+#     connection_string = f'mysql+pymysql://{st.secrets["username"]}:{st.secrets["password"]}@{st.secrets["host"]}:{st.secrets["port"]}/{st.secrets["database"]}'
+#     return create_engine(connection_string)
 
 
-def connect_sql():
-    connection_string = f'mysql+pymysql://{st.secrets["username"]}:{st.secrets["password"]}@{st.secrets["host"]}:{st.secrets["port"]}/{st.secrets["database"]}'
-    return create_engine(connection_string)
+
+def init_connect():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connect()
+
 
 @st.cache_data
 def load_data(query):
-    db_connect = connect_sql()
-    result = pd.read_sql(query, db_connect)
+    result = pd.read_sql(query, conn)
     return result
 
 # Main function Streamlit
